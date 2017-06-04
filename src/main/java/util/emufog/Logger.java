affb3eedf3a0d9ffa52ba322376bd7c1ffda3acc
unly <leongrch@gmail.com>
2017-06-04 17:13:37 +0200

    util package

---
+++ src/main/java/util/emufog/Logger.java
@@ -0,0 +1,117 @@
+package util.emufog;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * The logger class offers to write to the console and the log to save it at a later time.
+ * Different levels of log entries can be used. Uses singleton pattern.
+ */
+public class Logger {
+
+    /* unique instance of the logger class */
+    private static Logger INSTANCE;
+
+    /* lines of log entries to write to a file */
+    private final List<String> lines;
+
+    /* separator object to have a consistent look */
+    private final String separator;
+
+    /**
+     * Creates a new logger instance.
+     */
+    private Logger() {
+        lines = new ArrayList<>();
+        separator = "##############################################################";
+    }
+
+    /**
+     * Returns the unique logger instance. In case there is none yet the
+     * method creates a new object and returns it.
+     *
+     * @return the unique logger instance
+     */
+    public static Logger getInstance() {
+        if (INSTANCE == null) {
+            INSTANCE = new Logger();
+        }
+
+        return INSTANCE;
+    }
+
+    /**
+     * Write the given message to the console and stores it in the log to write it to an output file.
+     * Uses the INFO level as per default.
+     *
+     * @param msg message to append to the log
+     */
+    public void log(String msg) {
+        log(msg, LoggerLevel.INFO);
+    }
+
+    /**
+     * Write the given message to the console and stores it in the log to write it to an output file.
+     * Uses the specified logger level.
+     *
+     * @param msg   message to append to the log
+     * @param level priority level of the logger
+     */
+    public void log(String msg, LoggerLevel level) {
+        if (msg != null) {
+            System.out.println(msg);
+            lines.add(msg);
+        }
+    }
+
+    /**
+     * Adds a separator object with the given logger level to the log to increase readability.
+     *
+     * @param level level to apply to the separator
+     */
+    public void logSeparator(LoggerLevel level) {
+        log(separator, level);
+    }
+
+    /**
+     * Adds a separator object to the log to increase readability.
+     */
+    public void logSeparator() {
+        log(separator, LoggerLevel.INFO);
+    }
+
+    /**
+     * Converts a start and end point in nano seconds to a ms string to log.
+     *
+     * @param start start point in ns
+     * @param end   end point in ns
+     * @return string in ms format
+     */
+    public static String convertToMs(long start, long end) {
+        return (double)(end - start) / 1000000 + "ms";
+    }
+
+    /**
+     * Writes the log to the given file and resets it.
+     *
+     * @param path path to write the output to
+     */
+    public void saveLogFile(Path path) {
+        if (path == null) {
+            throw new IllegalArgumentException("The given path object is not initialized.");
+        }
+
+        try {
+            Files.write(path, lines, StandardCharsets.UTF_8, StandardOpenOption.CREATE,
+                    StandardOpenOption.TRUNCATE_EXISTING);
+            lines.clear();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/util/emufog/Tuple.java
@@ -0,0 +1,76 @@
+package util.emufog;
+
+/**
+ * This class represents a generic tuple consisting of two values.
+ * The types of values can be specified by K and V.
+ *
+ * @param <K> Class of the first value
+ * @param <V> Class of the second value
+ */
+public class Tuple<K, V> {
+
+    /* the key or the first value of the tuple */
+    private K key;
+
+    /* the second value of the tuple */
+    private V value;
+
+    /**
+     * Creates a new tuple with the given values.
+     * Both can be set to null at this point.
+     *
+     * @param key   first value
+     * @param value second value
+     */
+    public Tuple(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    /**
+     * Returns the first value of the tuple.
+     *
+     * @return first value of the tuple
+     */
+    public K getKey() {
+        return key;
+    }
+
+    /**
+     * Returns the second value of the tuple.
+     *
+     * @return second value of the tuple
+     */
+    public V getValue() {
+        return value;
+    }
+
+    /**
+     * Updates the key with the new given key value.
+     *
+     * @param key the new key to set
+     */
+    public void setKey(K key) {
+        this.key = key;
+    }
+
+    /**
+     * Updates the value of the tuple.
+     *
+     * @param value the new value to set
+     */
+    public void setValue(V value) {
+        this.value = value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        boolean result = false;
+
+        if (o instanceof Tuple) {
+            result = key.equals(((Tuple) o).key);
+        }
+
+        return result;
+    }
+}
