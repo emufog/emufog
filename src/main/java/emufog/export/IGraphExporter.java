1b496ed6cd9b8a4726139b183e3c7c4d6d045d5b
unly <leongrch@gmail.com>
2017-08-21 13:56:16 +0200

    add io exception for disk writing

--- src/main/java/emufog/export/IGraphExporter.java
+++ src/main/java/emufog/export/IGraphExporter.java
@@ -2,6 +2,7 @@ package emufog.export;
 
 import emufog.graph.Graph;
 
+import java.io.IOException;
 import java.nio.file.Path;
 
 public interface IGraphExporter {
@@ -12,6 +13,7 @@ public interface IGraphExporter {
      * @param graph the graph to export
      * @param path  path to export the graph to
      * @throws IllegalArgumentException throws exception if the input parameters are invalid
+     * @throws IOException              throws exception if an error occurs when writing to disk
      */
-    void exportGraph(Graph graph, Path path) throws IllegalArgumentException;
+    void exportGraph(Graph graph, Path path) throws IllegalArgumentException, IOException;
 }
