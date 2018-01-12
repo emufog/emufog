d2a6ea31d2de7a2a95a5e29aa056565f590b4965
Robin Finkbeiner <finkbero@gmail.com>
2018-01-12 17:27:27 +0100

    Created basic skeleton structure for Topology with methods read(), identifyEdge(), placeFogNodes(), assignApplications() and export().

---
+++ src/main/java/emufog/placement/DefaultFogLayout.java
@@ -0,0 +1,17 @@
+package emufog.placement;
+
+import com.google.common.graph.MutableNetwork;
+
+public class DefaultFogLayout implements IFogLayout{
+
+
+    @Override
+    public void identifyFogNodes(MutableNetwork topology) {
+
+    }
+
+    @Override
+    public void placeFogNodes(MutableNetwork topology) {
+
+    }
+}
