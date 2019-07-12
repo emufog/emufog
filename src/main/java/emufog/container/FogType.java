2f57e850bc910c7c6292594a99fab1166c2897ff
Leon Graser <leongrch@gmail.com>
2019-07-12 17:09:49 +0200

    refactored the docker package to container package

--- src/main/java/emufog/container/FogType.java
+++ src/main/java/emufog/container/FogType.java
@@ -21,16 +21,16 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-package emufog.docker;
+package emufog.container;
 
 import java.util.ArrayList;
 import java.util.List;
 
 /**
- * This docker image represents a fog computing node in the topology.
+ * This container image represents a fog computing node in the topology.
  * It can serve a fixed number of clients and is associated with deployment costs.
  */
-public class FogType extends DockerType {
+public class FogType extends ContainerType {
 
     /* maximal number of clients this image can serve, including */
     public final int maxClients;
@@ -44,16 +44,36 @@ public class FogType extends DockerType {
     /**
      * Creates a new fog computing node to be deployed in the network.
      *
-     * @param dockerImage actual docker image to deploy
-     * @param maxClients  maximum number of clients to serve
-     * @param costs       costs to deploy this image
-     * @param memoryLimit upper limit of memory to use in Bytes
-     * @param cpuShare    share of the sum of available computing resources
-     * @throws IllegalArgumentException the docker image name cannot be null and must
-     *                                  match the pattern of a docker container name
+     * @param containerName    name of the container image to deploy
+     * @param containerVersion version of the container image to deploy
+     * @param maxClients       maximum number of clients to serve
+     * @param costs            costs to deploy this image
+     * @param memoryLimit      upper limit of memory to use in Bytes
+     * @param cpuShare         share of the sum of available computing resources
+     * @throws IllegalArgumentException the container image name cannot be {@code null} and must
+     *                                  match the pattern of a container container name
      */
-    public FogType(String dockerImage, int maxClients, float costs, int memoryLimit, float cpuShare) throws IllegalArgumentException {
-        super(dockerImage, memoryLimit, cpuShare);
+    public FogType(String containerName, String containerVersion, int maxClients, float costs, int memoryLimit, float cpuShare) throws IllegalArgumentException {
+        super(containerName, containerVersion, memoryLimit, cpuShare);
+
+        this.maxClients = maxClients;
+        this.costs = costs;
+        dependencies = new ArrayList<>();
+    }
+
+    /**
+     * Creates a new fog computing node to be deployed in the network.
+     *
+     * @param containerName    name of the container image to deploy
+     * @param maxClients       maximum number of clients to serve
+     * @param costs            costs to deploy this image
+     * @param memoryLimit      upper limit of memory to use in Bytes
+     * @param cpuShare         share of the sum of available computing resources
+     * @throws IllegalArgumentException the container image name cannot be {@code null} and must
+     *                                  match the pattern of a container container name
+     */
+    public FogType(String containerName, int maxClients, float costs, int memoryLimit, float cpuShare) throws IllegalArgumentException {
+        super(containerName, memoryLimit, cpuShare);
 
         this.maxClients = maxClients;
         this.costs = costs;
@@ -64,7 +84,7 @@ public class FogType extends DockerType {
      * Adds
      *
      * @param type fog node type dependency to add
-     * @throws IllegalArgumentException if type object is null
+     * @throws IllegalArgumentException if type object is {@code null}
      */
     public void addDependency(FogType type) throws IllegalArgumentException {
         if (type == null) {
