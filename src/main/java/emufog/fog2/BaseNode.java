cc16f9ffc87792e888abbd5964713bebb33028c4
unly <leongrch@gmail.com>
2019-08-06 20:49:50 +0200

    adding license header to fog package

--- src/main/java/emufog/fog2/BaseNode.java
+++ src/main/java/emufog/fog2/BaseNode.java
@@ -1,3 +1,26 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2019 emufog contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package emufog.fog2;
 
 import emufog.container.FogType;
@@ -66,11 +89,12 @@ abstract class BaseNode {
     }
 
     List<Tuple<StartingNode, Integer>> getCoveredStartingNodes() {
-        List<StartingNode> startingNodes = costs.keySet().stream()
-                .map(k -> new Tuple<>(k, costs.get(k).getValue()))
-                .sorted((a, b) -> Float.compare(a.getValue(), b.getValue()))
-                .map(Tuple::getKey)
-                .collect(Collectors.toList());
+        List<StartingNode> startingNodes = costs.keySet()
+            .stream()
+            .map(k -> new Tuple<>(k, costs.get(k).getValue()))
+            .sorted((a, b) -> Float.compare(a.getValue(), b.getValue()))
+            .map(Tuple::getKey)
+            .collect(Collectors.toList());
 
         List<Tuple<StartingNode, Integer>> result = new ArrayList<>();
         int remaining = coveredCount;
--- src/main/java/emufog/fog2/FogWorker.java
+++ src/main/java/emufog/fog2/FogWorker.java
@@ -1,3 +1,26 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2019 emufog contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package emufog.fog2;
 
 import emufog.container.FogType;
@@ -35,7 +58,6 @@ class FogWorker {
 
     private final Comparator<BaseNode> comparator;
 
-
     FogWorker(AS as, FogNodeClassifier classifier) {
         this.as = as;
         this.classifier = classifier;
@@ -46,10 +68,7 @@ class FogWorker {
 
     FogResult processAS() {
         // map edge device nodes to their respective wrappers
-        List<StartingNode> startingNodes = as.getEdgeNodes().stream()
-                .filter(EdgeNode::hasDevices)
-                .map(this::createStartingNode)
-                .collect(Collectors.toList());
+        List<StartingNode> startingNodes = as.getEdgeNodes().stream().filter(EdgeNode::hasDevices).map(this::createStartingNode).collect(Collectors.toList());
         // calculate connection costs from the edge device nodes
         long start = System.nanoTime();
         startingNodes.forEach(this::calculateConnectionCosts);
