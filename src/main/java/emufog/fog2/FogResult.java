6a36424fd59f076d6035762113338c74d58b4e87
unly <leongrch@gmail.com>
2019-08-13 21:18:24 +0200

    Adds documentation to the fog result

--- src/main/java/emufog/fog2/FogResult.java
+++ src/main/java/emufog/fog2/FogResult.java
@@ -27,37 +27,82 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
+/**
+ * This class represents the outcome of a fog node placement algorithm.
+ * The {@link #status} represents the success or failure of the placement
+ * and {@link #placements} the individual fog node placements.
+ */
 public class FogResult {
 
+    /**
+     * the outcome of the result set
+     */
     private boolean status;
 
+    /**
+     * list of all fog node placements in the result set
+     */
     private final List<FogNodePlacement> placements;
 
+    /**
+     * Creates a new empty result object. Initially the outcome is {@code false} and
+     * there are no placements.
+     */
     FogResult() {
         status = false;
         placements = new ArrayList<>();
     }
 
+    /**
+     * Returns the outcome of the fog node placement.
+     * If {@code true} the {@link #getPlacements()} is returning the
+     * correct result. For {@code false} {@link #getPlacements()} can
+     * be any intermediate result.
+     *
+     * @return outcome status of the fog node placement
+     */
     public boolean getStatus() {
         return status;
     }
 
+    /**
+     * Returns the list of fog node placements associated with this fog node placement
+     * result object.
+     *
+     * @return list off all fog node placements identified
+     */
     public List<FogNodePlacement> getPlacements() {
         return placements;
     }
 
+    /**
+     * Adds a new fog node placement to the list of the result object.
+     *
+     * @param placement fog node placement to add
+     */
     void addPlacement(FogNodePlacement placement) {
         placements.add(placement);
     }
 
+    /**
+     * Adds a collection of fog node placements to the list of the result object.
+     *
+     * @param placements collection of fog node placements to add
+     */
     void addPlacements(Collection<FogNodePlacement> placements) {
         this.placements.addAll(placements);
     }
 
+    /**
+     * Sets the {@link #status} of the result object to {@code true}.
+     */
     void setSuccess() {
         status = true;
     }
 
+    /**
+     * Sets the {@link #status} of the result object to {@code false}.
+     */
     void setFailure() {
         status = false;
     }
