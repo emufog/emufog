98ee1cd8b5ddebf859f2dd803d9b8eac26299181
unly <leongrch@gmail.com>
2019-10-31 22:16:30 +0100

    fix backbone package

--- src/main/java/emufog/backbone/BackboneClassifier.java
+++ src/main/java/emufog/backbone/BackboneClassifier.java
@@ -21,7 +21,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-package backbone;
+package emufog.backbone;
 
 import emufog.graph.AS;
 import emufog.graph.BackboneNodeConverter;
